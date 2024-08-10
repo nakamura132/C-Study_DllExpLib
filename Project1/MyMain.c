88b20566d0842757b47f5f12aaad58eb22b4461a
nakamura132 <windpublic@gmail.com>
2024-08-11 05:55:56 +0900

    明示的リンク(Implicit link)

--- Project1/MyMain.c
+++ Project1/MyMain.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
-#include "MyLibInc.h"
+#include <windows.h>
+
+typedef int (*MYINC)(int i);
 
 int main()
 {
     printf("Start\n");
 
-    int i = MyInc(3); // ★ 「プラス 1 する関数」の呼び出し
+    // DLL を明示的にロードする
+    HMODULE hmodule = LoadLibrary("MyLibInc.dll");
+    if (hmodule == NULL)
+    {
+        printf("LoadLibrary, err=%d\n", GetLastError());
+        return 1;
+    }
+
+    // DLL の中にある特定の関数のアドレスを取得する
+    MYINC fnMyInc = (MYINC)GetProcAddress(hmodule, "MyInc");
+    if (fnMyInc == NULL)
+    {
+        printf("GetProcAddress, err=%d\n", GetLastError());
+        return 1;
+    }
+
+    // DLL の中にある特定の関数を呼び出す
+    int i = fnMyInc(7);
     printf("i=%d\n", i);
+
+    // DLL をアンロードする
+    BOOL bSuccess = FreeLibrary(hmodule);
+    if (!bSuccess)
+    {
+        printf("FreeLibrary, err=%d\n", GetLastError());
+        return 1;
+    }
 
     return 0;
 }
